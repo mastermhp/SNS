12209799e377a0ae67d2675e1a00b0143edef6b7
Taki Uddin <takiuddin@Mac-2.powerhub>
2025-07-10 00:36:56 +0100

    transition error fix and favicon changes

--- src/app/components/ContactSection.js
+++ src/app/components/ContactSection.js
@@ -132,18 +132,15 @@ export default function ContactSection() {
                 }}
                 initial={{ opacity: 1, scale: 1, y: 50 }}
                 whileInView={{ opacity: 1, scale: 1, y: 0 }}
-                transition={{
-                  duration: 0.8,
-                  delay: item.delay,
-                  type: "spring",
-                  stiffness: 100,
-                }}
                 animate={{
                   y: [0, -8, 0],
                   rotate: item.rotate ? [item.rotate, item.rotate + 5, item.rotate - 5, item.rotate] : [0, 2, -2, 0],
                 }}
                 transition={{
-                  duration: 4,
+                  duration: 0.8,
+                  delay: item.delay,
+                  type: "spring",
+                  stiffness: 100,
                   repeat: Number.POSITIVE_INFINITY,
                   ease: "easeInOut",
                 }}
