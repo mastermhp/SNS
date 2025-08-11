8104fed9709c168ac3fb073b7c8dce2ec4b6ec8e
Taki Uddin <takiuddin93@gmail.com>
2025-08-11 19:19:49 +0100

    Add Google Analytics, robots.txt, and sitemap.xml for SEO optimization

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,6 +13,18 @@ export default function RootLayout({ children }) {
           href="https://api.fontshare.com/v2/css?f[]=bebas-neue@400&display=swap"
           rel="stylesheet"
         />
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-XL5SWYB0WF"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-XL5SWYB0WF');
+            `,
+          }}
+        />
       </head>
       <body className="bg-black text-white overflow-x-hidden" style={{ fontFamily: "General Sans, sans-serif" }}>
         {children}
